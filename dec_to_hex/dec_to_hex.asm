1daae0b92cb6278636caa8172b17cea035c9907d
cbasile <cbasile@zixcorp.com>
2017-05-05 15:02:56 -0500

    Updating dec_to_hex

--- dec_to_hex/dec_to_hex.asm
+++ dec_to_hex/dec_to_hex.asm
@@ -3,24 +3,50 @@ section .data
 msg: db "Enter your number:", 10
 len: equ $-msg
 section .bss
-str1: resb 1 
+str1: resb 10 
 section .text
 start:
 	push	dword len	;Length of message
 	push	dword msg	;Message to write
 	push	dword 1		;STDOUT
-	sub 	esp,4		;Make space on stack
 	mov 	eax,4		;We are writing
+	sub 	esp 4		;Making space on the stack??	
 	int 	0x80		;SYSCALL
-	pop 	ebx	
+	add 	esp,16		;Move back
 	
-	push 	dword 1		;Once again length
-	push 	dword ebx	;msg
-	push 	dword 1		;stout
-	sub	esp,4
-	mov 	eax,4		;Read
-	int 	0x80	
+	push 	dword 10	;length of message (10)
+	push	dword str1	;Place to put it
+	push 	dword 0		;STDIN 0. Not 3.
+	mov 	eax,3		;We are reading
+	sub	esp,4		;push	eax
+	int 	0x80
+	add	esp,16
+	
+	mov	ebx, str1	;Move it around
+	push	dword 10	;Printing 10
+	push	dword ebx	;Hopefully putting the string back on the stack?
+	push	dword 1		;STDOUT
+	mov	eax,4
+	sub	esp,4		
+	int 	0x80
 
+	;Okay now we have ASCII characters. 
+	;I would like to write a function to print out the value on the stack as a binary number, just to see it.
+	push ebx
+	call printBinary
+	call end
+
+printBinary:
+	push	ebp
+	mov	ebp, esp
+	mov 	esi, [esp+8]
+		
+	
+
+	mov	esp, ebp
+	pop 	ebp
+	ret
+end:
 	add 	esp,16		;Move back
 	push 	dword 0		;Exit code
 	mov 	eax,1		;Exit command
